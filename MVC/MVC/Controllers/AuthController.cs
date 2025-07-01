e6011bcbd042aa6be2593a34259350dff586af59
brthagdis <bertha.gadis@gmail.com>
2025-07-01 21:01:48 +0700

    update mvc

---
+++ MVC/MVC/Controllers/AuthController.cs
@@ -0,0 +1,24 @@
+﻿using dietapp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dietapp.Controllers
+{
+    public class AuthController
+    {
+        private readonly UserModel _userModel;
+
+        public AuthController()
+        {
+            _userModel = new UserModel();
+        }
+
+        public bool Login(string username, string password)
+        {
+            return _userModel.Authenticate(username, password);
+        }
+    }
+}
