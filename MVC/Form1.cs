29c68fa91e0c8b46621b495bbb63c7fb2f5f5e99
brthagdis <bertha.gadis@gmail.com>
2025-07-01 20:56:41 +0700

    upload mvc

---
+++ MVC/Form1.cs
@@ -0,0 +1,55 @@
+﻿using dietapp.Controllers;
+using MySql.Data.MySqlClient;
+using System;
+using System.Windows.Forms;
+
+namespace dietapp
+{
+    public partial class LoginForm : Form
+    {
+        private readonly AuthController _authController;
+        public LoginForm()
+        {
+            InitializeComponent();
+            _authController = new AuthController();
+        }
+
+        private void LoginForm_Load(object sender, EventArgs e)
+        {
+            txtPassword.UseSystemPasswordChar = true;
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string username = txtUsername.Text.Trim();
+            string password = txtPassword.Text.Trim();
+
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+            {
+                MessageBox.Show("Silakan isi username dan password.", "Peringatan", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            try
+            {
+                bool isLoginValid = _authController.Login(username, password);
+
+                if (isLoginValid)
+                {
+                    MessageBox.Show("Login berhasil!", "Sukses", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    InputData formUtama = new InputData();
+                    formUtama.Show();
+                    this.Hide();
+                }
+                else
+                {
+                    MessageBox.Show("Username atau password salah!", "Gagal", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Terjadi kesalahan koneksi database: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
